b0dca2e56612ab3b0183ca9bf02195355fcdcdd7
rainiefch <143920728+rainiefch@users.noreply.github.com>
2025-02-05 20:01:18 +0700

    Add files via upload

---
+++ Folder_Pertemuan_1/Calculator_71230982/Calculator_71230982/Form1.Designer.vb
@@ -0,0 +1,376 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        txtInput = New TextBox()
+        Panel2 = New Panel()
+        cmdBackspace = New Button()
+        cmdDivision = New Button()
+        cmdEqual = New Button()
+        cmd7 = New Button()
+        cmdAddition = New Button()
+        cmd8 = New Button()
+        cmdC = New Button()
+        cmd9 = New Button()
+        cmdCE = New Button()
+        cmd4 = New Button()
+        cmdInverse = New Button()
+        cmd5 = New Button()
+        cmdSubstract = New Button()
+        cmd6 = New Button()
+        cmdCbrt = New Button()
+        cmd1 = New Button()
+        cmdMultiply = New Button()
+        cmd2 = New Button()
+        cmdSqrt = New Button()
+        cmd3 = New Button()
+        cmd0 = New Button()
+        cmdPowerOf = New Button()
+        cmdSign = New Button()
+        cmdFactorial = New Button()
+        cmdDecimal = New Button()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(txtInput)
+        Panel1.Location = New Point(27, 25)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(431, 108)
+        Panel1.TabIndex = 0
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Font = New Font("Segoe UI", 18F)
+        txtInput.Location = New Point(19, 30)
+        txtInput.Name = "txtInput"
+        txtInput.Size = New Size(390, 55)
+        txtInput.TabIndex = 0
+        txtInput.Text = "0"
+        txtInput.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(cmdBackspace)
+        Panel2.Controls.Add(cmdDivision)
+        Panel2.Controls.Add(cmdEqual)
+        Panel2.Controls.Add(cmd7)
+        Panel2.Controls.Add(cmdAddition)
+        Panel2.Controls.Add(cmd8)
+        Panel2.Controls.Add(cmdC)
+        Panel2.Controls.Add(cmd9)
+        Panel2.Controls.Add(cmdCE)
+        Panel2.Controls.Add(cmd4)
+        Panel2.Controls.Add(cmdInverse)
+        Panel2.Controls.Add(cmd5)
+        Panel2.Controls.Add(cmdSubstract)
+        Panel2.Controls.Add(cmd6)
+        Panel2.Controls.Add(cmdCbrt)
+        Panel2.Controls.Add(cmd1)
+        Panel2.Controls.Add(cmdMultiply)
+        Panel2.Controls.Add(cmd2)
+        Panel2.Controls.Add(cmdSqrt)
+        Panel2.Controls.Add(cmd3)
+        Panel2.Controls.Add(cmd0)
+        Panel2.Controls.Add(cmdPowerOf)
+        Panel2.Controls.Add(cmdSign)
+        Panel2.Controls.Add(cmdFactorial)
+        Panel2.Controls.Add(cmdDecimal)
+        Panel2.Location = New Point(27, 151)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(431, 435)
+        Panel2.TabIndex = 1
+        ' 
+        ' cmdBackspace
+        ' 
+        cmdBackspace.Location = New Point(19, 86)
+        cmdBackspace.Name = "cmdBackspace"
+        cmdBackspace.Size = New Size(146, 61)
+        cmdBackspace.TabIndex = 25
+        cmdBackspace.Text = "Backspace"
+        cmdBackspace.UseVisualStyleBackColor = True
+        ' 
+        ' cmdDivision
+        ' 
+        cmdDivision.Location = New Point(263, 153)
+        cmdDivision.Name = "cmdDivision"
+        cmdDivision.Size = New Size(70, 61)
+        cmdDivision.TabIndex = 15
+        cmdDivision.Text = "/"
+        cmdDivision.UseVisualStyleBackColor = True
+        ' 
+        ' cmdEqual
+        ' 
+        cmdEqual.Location = New Point(339, 354)
+        cmdEqual.Name = "cmdEqual"
+        cmdEqual.Size = New Size(70, 61)
+        cmdEqual.TabIndex = 24
+        cmdEqual.Text = "="
+        cmdEqual.UseVisualStyleBackColor = True
+        ' 
+        ' cmd7
+        ' 
+        cmd7.Location = New Point(19, 153)
+        cmd7.Name = "cmd7"
+        cmd7.Size = New Size(70, 61)
+        cmd7.TabIndex = 0
+        cmd7.Text = "7"
+        cmd7.UseVisualStyleBackColor = True
+        ' 
+        ' cmdAddition
+        ' 
+        cmdAddition.Location = New Point(263, 354)
+        cmdAddition.Name = "cmdAddition"
+        cmdAddition.Size = New Size(70, 61)
+        cmdAddition.TabIndex = 23
+        cmdAddition.Text = "+"
+        cmdAddition.UseVisualStyleBackColor = True
+        ' 
+        ' cmd8
+        ' 
+        cmd8.Location = New Point(95, 153)
+        cmd8.Name = "cmd8"
+        cmd8.Size = New Size(70, 61)
+        cmd8.TabIndex = 2
+        cmd8.Text = "8"
+        cmd8.UseVisualStyleBackColor = True
+        ' 
+        ' cmdC
+        ' 
+        cmdC.Location = New Point(95, 19)
+        cmdC.Name = "cmdC"
+        cmdC.Size = New Size(70, 61)
+        cmdC.TabIndex = 22
+        cmdC.Text = "C"
+        cmdC.UseVisualStyleBackColor = True
+        ' 
+        ' cmd9
+        ' 
+        cmd9.Location = New Point(171, 153)
+        cmd9.Name = "cmd9"
+        cmd9.Size = New Size(70, 61)
+        cmd9.TabIndex = 3
+        cmd9.Text = "9"
+        cmd9.UseVisualStyleBackColor = True
+        ' 
+        ' cmdCE
+        ' 
+        cmdCE.Location = New Point(19, 19)
+        cmdCE.Name = "cmdCE"
+        cmdCE.Size = New Size(70, 61)
+        cmdCE.TabIndex = 21
+        cmdCE.Text = "CE"
+        cmdCE.UseVisualStyleBackColor = True
+        ' 
+        ' cmd4
+        ' 
+        cmd4.Location = New Point(19, 220)
+        cmd4.Name = "cmd4"
+        cmd4.Size = New Size(70, 61)
+        cmd4.TabIndex = 4
+        cmd4.Text = "4"
+        cmd4.UseVisualStyleBackColor = True
+        ' 
+        ' cmdInverse
+        ' 
+        cmdInverse.Location = New Point(339, 287)
+        cmdInverse.Name = "cmdInverse"
+        cmdInverse.Size = New Size(70, 61)
+        cmdInverse.TabIndex = 20
+        cmdInverse.Text = "1/X"
+        cmdInverse.UseVisualStyleBackColor = True
+        ' 
+        ' cmd5
+        ' 
+        cmd5.Location = New Point(95, 220)
+        cmd5.Name = "cmd5"
+        cmd5.Size = New Size(70, 61)
+        cmd5.TabIndex = 5
+        cmd5.Text = "5"
+        cmd5.UseVisualStyleBackColor = True
+        ' 
+        ' cmdSubstract
+        ' 
+        cmdSubstract.Location = New Point(263, 287)
+        cmdSubstract.Name = "cmdSubstract"
+        cmdSubstract.Size = New Size(70, 61)
+        cmdSubstract.TabIndex = 19
+        cmdSubstract.Text = "-"
+        cmdSubstract.UseVisualStyleBackColor = True
+        ' 
+        ' cmd6
+        ' 
+        cmd6.Location = New Point(171, 220)
+        cmd6.Name = "cmd6"
+        cmd6.Size = New Size(70, 61)
+        cmd6.TabIndex = 6
+        cmd6.Text = "6"
+        cmd6.UseVisualStyleBackColor = True
+        ' 
+        ' cmdCbrt
+        ' 
+        cmdCbrt.Location = New Point(339, 220)
+        cmdCbrt.Name = "cmdCbrt"
+        cmdCbrt.Size = New Size(70, 61)
+        cmdCbrt.TabIndex = 18
+        cmdCbrt.Text = "CBRT"
+        cmdCbrt.UseVisualStyleBackColor = True
+        ' 
+        ' cmd1
+        ' 
+        cmd1.Location = New Point(19, 287)
+        cmd1.Name = "cmd1"
+        cmd1.Size = New Size(70, 61)
+        cmd1.TabIndex = 7
+        cmd1.Text = "1"
+        cmd1.UseVisualStyleBackColor = True
+        ' 
+        ' cmdMultiply
+        ' 
+        cmdMultiply.Location = New Point(263, 220)
+        cmdMultiply.Name = "cmdMultiply"
+        cmdMultiply.Size = New Size(70, 61)
+        cmdMultiply.TabIndex = 17
+        cmdMultiply.Text = "*"
+        cmdMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' cmd2
+        ' 
+        cmd2.Location = New Point(95, 287)
+        cmd2.Name = "cmd2"
+        cmd2.Size = New Size(70, 61)
+        cmd2.TabIndex = 8
+        cmd2.Text = "2"
+        cmd2.UseVisualStyleBackColor = True
+        ' 
+        ' cmdSqrt
+        ' 
+        cmdSqrt.Location = New Point(339, 153)
+        cmdSqrt.Name = "cmdSqrt"
+        cmdSqrt.Size = New Size(70, 61)
+        cmdSqrt.TabIndex = 16
+        cmdSqrt.Text = "SQRT"
+        cmdSqrt.UseVisualStyleBackColor = True
+        ' 
+        ' cmd3
+        ' 
+        cmd3.Location = New Point(171, 287)
+        cmd3.Name = "cmd3"
+        cmd3.Size = New Size(70, 61)
+        cmd3.TabIndex = 9
+        cmd3.Text = "3"
+        cmd3.UseVisualStyleBackColor = True
+        ' 
+        ' cmd0
+        ' 
+        cmd0.Location = New Point(19, 354)
+        cmd0.Name = "cmd0"
+        cmd0.Size = New Size(70, 61)
+        cmd0.TabIndex = 10
+        cmd0.Text = "0"
+        cmd0.UseVisualStyleBackColor = True
+        ' 
+        ' cmdPowerOf
+        ' 
+        cmdPowerOf.Location = New Point(339, 86)
+        cmdPowerOf.Name = "cmdPowerOf"
+        cmdPowerOf.Size = New Size(70, 61)
+        cmdPowerOf.TabIndex = 14
+        cmdPowerOf.Text = "X^"
+        cmdPowerOf.UseVisualStyleBackColor = True
+        ' 
+        ' cmdSign
+        ' 
+        cmdSign.Location = New Point(95, 354)
+        cmdSign.Name = "cmdSign"
+        cmdSign.Size = New Size(70, 61)
+        cmdSign.TabIndex = 11
+        cmdSign.Text = "+/-"
+        cmdSign.UseVisualStyleBackColor = True
+        ' 
+        ' cmdFactorial
+        ' 
+        cmdFactorial.Location = New Point(263, 86)
+        cmdFactorial.Name = "cmdFactorial"
+        cmdFactorial.Size = New Size(70, 61)
+        cmdFactorial.TabIndex = 13
+        cmdFactorial.Text = "n!"
+        cmdFactorial.UseVisualStyleBackColor = True
+        ' 
+        ' cmdDecimal
+        ' 
+        cmdDecimal.Location = New Point(171, 354)
+        cmdDecimal.Name = "cmdDecimal"
+        cmdDecimal.Size = New Size(70, 61)
+        cmdDecimal.TabIndex = 12
+        cmdDecimal.Text = "."
+        cmdDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(483, 611)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents cmd7 As Button
+    Friend WithEvents cmd8 As Button
+    Friend WithEvents cmd9 As Button
+    Friend WithEvents cmd6 As Button
+    Friend WithEvents cmd5 As Button
+    Friend WithEvents cmd4 As Button
+    Friend WithEvents cmd3 As Button
+    Friend WithEvents cmd2 As Button
+    Friend WithEvents cmd1 As Button
+    Friend WithEvents cmdDecimal As Button
+    Friend WithEvents cmdSign As Button
+    Friend WithEvents cmd0 As Button
+    Friend WithEvents cmdInverse As Button
+    Friend WithEvents cmdSubstract As Button
+    Friend WithEvents cmdCbrt As Button
+    Friend WithEvents cmdMultiply As Button
+    Friend WithEvents cmdSqrt As Button
+    Friend WithEvents cmdDivision As Button
+    Friend WithEvents cmdPowerOf As Button
+    Friend WithEvents cmdFactorial As Button
+    Friend WithEvents cmdC As Button
+    Friend WithEvents cmdCE As Button
+    Friend WithEvents cmdBackspace As Button
+    Friend WithEvents cmdEqual As Button
+    Friend WithEvents cmdAddition As Button
+
+End Class
