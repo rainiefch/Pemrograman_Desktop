c369eb3d8cdfc131b0944c6a11ec5034c49ba687
rainiefch <143920728+rainiefch@users.noreply.github.com>
2025-02-05 16:19:36 +0700

    Add files via upload

---
+++ Pertemuan_1/Kalkulator2_71230982/Form1.Designer.vb
@@ -0,0 +1,388 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmd7 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.cmdCE = New System.Windows.Forms.Button()
+        Me.cmdC = New System.Windows.Forms.Button()
+        Me.cmdEqual = New System.Windows.Forms.Button()
+        Me.cmdBackspace = New System.Windows.Forms.Button()
+        Me.cmdDecimal = New System.Windows.Forms.Button()
+        Me.cmdAddition = New System.Windows.Forms.Button()
+        Me.cmd0 = New System.Windows.Forms.Button()
+        Me.cmdSign = New System.Windows.Forms.Button()
+        Me.cmdInverse = New System.Windows.Forms.Button()
+        Me.cmd3 = New System.Windows.Forms.Button()
+        Me.cmdSubstraction = New System.Windows.Forms.Button()
+        Me.cmd2 = New System.Windows.Forms.Button()
+        Me.cmd1 = New System.Windows.Forms.Button()
+        Me.cmdSqrt = New System.Windows.Forms.Button()
+        Me.cmdDivision = New System.Windows.Forms.Button()
+        Me.cmd6 = New System.Windows.Forms.Button()
+        Me.cmdMultiply = New System.Windows.Forms.Button()
+        Me.cmd5 = New System.Windows.Forms.Button()
+        Me.cmdPowerOf = New System.Windows.Forms.Button()
+        Me.cmd4 = New System.Windows.Forms.Button()
+        Me.cmd9 = New System.Windows.Forms.Button()
+        Me.cmd8 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.cmdAkarPangkat3 = New System.Windows.Forms.Button()
+        Me.cmdFactorial = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmd7
+        '
+        Me.cmd7.Location = New System.Drawing.Point(17, 94)
+        Me.cmd7.Name = "cmd7"
+        Me.cmd7.Size = New System.Drawing.Size(75, 71)
+        Me.cmd7.TabIndex = 0
+        Me.cmd7.Text = "7"
+        Me.cmd7.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.txtInput)
+        Me.Panel1.Location = New System.Drawing.Point(21, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(467, 100)
+        Me.Panel1.TabIndex = 1
+        '
+        'txtInput
+        '
+        Me.txtInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.txtInput.Location = New System.Drawing.Point(17, 26)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(428, 48)
+        Me.txtInput.TabIndex = 0
+        Me.txtInput.Text = "0"
+        Me.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.cmdAkarPangkat3)
+        Me.Panel2.Controls.Add(Me.cmdFactorial)
+        Me.Panel2.Controls.Add(Me.cmdCE)
+        Me.Panel2.Controls.Add(Me.cmdC)
+        Me.Panel2.Controls.Add(Me.cmdEqual)
+        Me.Panel2.Controls.Add(Me.cmdBackspace)
+        Me.Panel2.Controls.Add(Me.cmdDecimal)
+        Me.Panel2.Controls.Add(Me.cmdAddition)
+        Me.Panel2.Controls.Add(Me.cmd0)
+        Me.Panel2.Controls.Add(Me.cmdSign)
+        Me.Panel2.Controls.Add(Me.cmdInverse)
+        Me.Panel2.Controls.Add(Me.cmd3)
+        Me.Panel2.Controls.Add(Me.cmdSubstraction)
+        Me.Panel2.Controls.Add(Me.cmd2)
+        Me.Panel2.Controls.Add(Me.cmd1)
+        Me.Panel2.Controls.Add(Me.cmdSqrt)
+        Me.Panel2.Controls.Add(Me.cmdDivision)
+        Me.Panel2.Controls.Add(Me.cmd6)
+        Me.Panel2.Controls.Add(Me.cmdMultiply)
+        Me.Panel2.Controls.Add(Me.cmd5)
+        Me.Panel2.Controls.Add(Me.cmdPowerOf)
+        Me.Panel2.Controls.Add(Me.cmd4)
+        Me.Panel2.Controls.Add(Me.cmd9)
+        Me.Panel2.Controls.Add(Me.cmd8)
+        Me.Panel2.Controls.Add(Me.cmd7)
+        Me.Panel2.Location = New System.Drawing.Point(21, 118)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(467, 494)
+        Me.Panel2.TabIndex = 2
+        '
+        'cmdCE
+        '
+        Me.cmdCE.Location = New System.Drawing.Point(289, 17)
+        Me.cmdCE.Name = "cmdCE"
+        Me.cmdCE.Size = New System.Drawing.Size(75, 71)
+        Me.cmdCE.TabIndex = 21
+        Me.cmdCE.Text = "CE"
+        Me.cmdCE.UseVisualStyleBackColor = True
+        '
+        'cmdC
+        '
+        Me.cmdC.Location = New System.Drawing.Point(370, 17)
+        Me.cmdC.Name = "cmdC"
+        Me.cmdC.Size = New System.Drawing.Size(75, 71)
+        Me.cmdC.TabIndex = 22
+        Me.cmdC.Text = "C"
+        Me.cmdC.UseVisualStyleBackColor = True
+        '
+        'cmdEqual
+        '
+        Me.cmdEqual.Location = New System.Drawing.Point(289, 402)
+        Me.cmdEqual.Name = "cmdEqual"
+        Me.cmdEqual.Size = New System.Drawing.Size(75, 71)
+        Me.cmdEqual.TabIndex = 20
+        Me.cmdEqual.Text = "="
+        Me.cmdEqual.UseVisualStyleBackColor = True
+        '
+        'cmdBackspace
+        '
+        Me.cmdBackspace.Location = New System.Drawing.Point(17, 17)
+        Me.cmdBackspace.Name = "cmdBackspace"
+        Me.cmdBackspace.Size = New System.Drawing.Size(156, 71)
+        Me.cmdBackspace.TabIndex = 13
+        Me.cmdBackspace.Text = "Backspace"
+        Me.cmdBackspace.UseVisualStyleBackColor = True
+        '
+        'cmdDecimal
+        '
+        Me.cmdDecimal.Location = New System.Drawing.Point(179, 325)
+        Me.cmdDecimal.Name = "cmdDecimal"
+        Me.cmdDecimal.Size = New System.Drawing.Size(75, 71)
+        Me.cmdDecimal.TabIndex = 11
+        Me.cmdDecimal.Text = "."
+        Me.cmdDecimal.UseVisualStyleBackColor = True
+        '
+        'cmdAddition
+        '
+        Me.cmdAddition.Location = New System.Drawing.Point(289, 325)
+        Me.cmdAddition.Name = "cmdAddition"
+        Me.cmdAddition.Size = New System.Drawing.Size(75, 71)
+        Me.cmdAddition.TabIndex = 19
+        Me.cmdAddition.Text = "+"
+        Me.cmdAddition.UseVisualStyleBackColor = True
+        '
+        'cmd0
+        '
+        Me.cmd0.Location = New System.Drawing.Point(17, 325)
+        Me.cmd0.Name = "cmd0"
+        Me.cmd0.Size = New System.Drawing.Size(75, 71)
+        Me.cmd0.TabIndex = 12
+        Me.cmd0.Text = "0"
+        Me.cmd0.UseVisualStyleBackColor = True
+        '
+        'cmdSign
+        '
+        Me.cmdSign.Location = New System.Drawing.Point(98, 325)
+        Me.cmdSign.Name = "cmdSign"
+        Me.cmdSign.Size = New System.Drawing.Size(75, 71)
+        Me.cmdSign.TabIndex = 10
+        Me.cmdSign.Text = "+/-"
+        Me.cmdSign.UseVisualStyleBackColor = True
+        '
+        'cmdInverse
+        '
+        Me.cmdInverse.Location = New System.Drawing.Point(370, 248)
+        Me.cmdInverse.Name = "cmdInverse"
+        Me.cmdInverse.Size = New System.Drawing.Size(75, 71)
+        Me.cmdInverse.TabIndex = 18
+        Me.cmdInverse.Text = "1/X"
+        Me.cmdInverse.UseVisualStyleBackColor = True
+        '
+        'cmd3
+        '
+        Me.cmd3.Location = New System.Drawing.Point(179, 248)
+        Me.cmd3.Name = "cmd3"
+        Me.cmd3.Size = New System.Drawing.Size(75, 71)
+        Me.cmd3.TabIndex = 8
+        Me.cmd3.Text = "3"
+        Me.cmd3.UseVisualStyleBackColor = True
+        '
+        'cmdSubstraction
+        '
+        Me.cmdSubstraction.Location = New System.Drawing.Point(289, 248)
+        Me.cmdSubstraction.Name = "cmdSubstraction"
+        Me.cmdSubstraction.Size = New System.Drawing.Size(75, 71)
+        Me.cmdSubstraction.TabIndex = 17
+        Me.cmdSubstraction.Text = "-"
+        Me.cmdSubstraction.UseVisualStyleBackColor = True
+        '
+        'cmd2
+        '
+        Me.cmd2.Location = New System.Drawing.Point(98, 248)
+        Me.cmd2.Name = "cmd2"
+        Me.cmd2.Size = New System.Drawing.Size(75, 71)
+        Me.cmd2.TabIndex = 7
+        Me.cmd2.Text = "2"
+        Me.cmd2.UseVisualStyleBackColor = True
+        '
+        'cmd1
+        '
+        Me.cmd1.Location = New System.Drawing.Point(17, 248)
+        Me.cmd1.Name = "cmd1"
+        Me.cmd1.Size = New System.Drawing.Size(75, 71)
+        Me.cmd1.TabIndex = 6
+        Me.cmd1.Text = "1"
+        Me.cmd1.UseVisualStyleBackColor = True
+        '
+        'cmdSqrt
+        '
+        Me.cmdSqrt.Location = New System.Drawing.Point(370, 171)
+        Me.cmdSqrt.Name = "cmdSqrt"
+        Me.cmdSqrt.Size = New System.Drawing.Size(75, 71)
+        Me.cmdSqrt.TabIndex = 16
+        Me.cmdSqrt.Text = "SQRT"
+        Me.cmdSqrt.UseVisualStyleBackColor = True
+        '
+        'cmdDivision
+        '
+        Me.cmdDivision.Location = New System.Drawing.Point(289, 94)
+        Me.cmdDivision.Name = "cmdDivision"
+        Me.cmdDivision.Size = New System.Drawing.Size(75, 71)
+        Me.cmdDivision.TabIndex = 13
+        Me.cmdDivision.Text = "/"
+        Me.cmdDivision.UseVisualStyleBackColor = True
+        '
+        'cmd6
+        '
+        Me.cmd6.Location = New System.Drawing.Point(179, 171)
+        Me.cmd6.Name = "cmd6"
+        Me.cmd6.Size = New System.Drawing.Size(75, 71)
+        Me.cmd6.TabIndex = 5
+        Me.cmd6.Text = "6"
+        Me.cmd6.UseVisualStyleBackColor = True
+        '
+        'cmdMultiply
+        '
+        Me.cmdMultiply.Location = New System.Drawing.Point(289, 171)
+        Me.cmdMultiply.Name = "cmdMultiply"
+        Me.cmdMultiply.Size = New System.Drawing.Size(75, 71)
+        Me.cmdMultiply.TabIndex = 15
+        Me.cmdMultiply.Text = "*"
+        Me.cmdMultiply.UseVisualStyleBackColor = True
+        '
+        'cmd5
+        '
+        Me.cmd5.Location = New System.Drawing.Point(98, 171)
+        Me.cmd5.Name = "cmd5"
+        Me.cmd5.Size = New System.Drawing.Size(75, 71)
+        Me.cmd5.TabIndex = 4
+        Me.cmd5.Text = "5"
+        Me.cmd5.UseVisualStyleBackColor = True
+        '
+        'cmdPowerOf
+        '
+        Me.cmdPowerOf.Location = New System.Drawing.Point(370, 94)
+        Me.cmdPowerOf.Name = "cmdPowerOf"
+        Me.cmdPowerOf.Size = New System.Drawing.Size(75, 71)
+        Me.cmdPowerOf.TabIndex = 14
+        Me.cmdPowerOf.Text = "X^"
+        Me.cmdPowerOf.UseVisualStyleBackColor = True
+        '
+        'cmd4
+        '
+        Me.cmd4.Location = New System.Drawing.Point(17, 171)
+        Me.cmd4.Name = "cmd4"
+        Me.cmd4.Size = New System.Drawing.Size(75, 71)
+        Me.cmd4.TabIndex = 3
+        Me.cmd4.Text = "4"
+        Me.cmd4.UseVisualStyleBackColor = True
+        '
+        'cmd9
+        '
+        Me.cmd9.Location = New System.Drawing.Point(179, 94)
+        Me.cmd9.Name = "cmd9"
+        Me.cmd9.Size = New System.Drawing.Size(75, 71)
+        Me.cmd9.TabIndex = 2
+        Me.cmd9.Text = "9"
+        Me.cmd9.UseVisualStyleBackColor = True
+        '
+        'cmd8
+        '
+        Me.cmd8.Location = New System.Drawing.Point(98, 94)
+        Me.cmd8.Name = "cmd8"
+        Me.cmd8.Size = New System.Drawing.Size(75, 71)
+        Me.cmd8.TabIndex = 1
+        Me.cmd8.Text = "8"
+        Me.cmd8.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(58, 375)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 71)
+        Me.Button12.TabIndex = 9
+        Me.Button12.Text = "Button12"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'cmdAkarPangkat3
+        '
+        Me.cmdAkarPangkat3.Location = New System.Drawing.Point(370, 402)
+        Me.cmdAkarPangkat3.Name = "cmdAkarPangkat3"
+        Me.cmdAkarPangkat3.Size = New System.Drawing.Size(75, 71)
+        Me.cmdAkarPangkat3.TabIndex = 24
+        Me.cmdAkarPangkat3.Text = "Akar^3"
+        Me.cmdAkarPangkat3.UseVisualStyleBackColor = True
+        '
+        'cmdFactorial
+        '
+        Me.cmdFactorial.Location = New System.Drawing.Point(370, 325)
+        Me.cmdFactorial.Name = "cmdFactorial"
+        Me.cmdFactorial.Size = New System.Drawing.Size(75, 71)
+        Me.cmdFactorial.TabIndex = 23
+        Me.cmdFactorial.Text = "n"
+        Me.cmdFactorial.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(509, 606)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmd7 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents cmdCE As Button
+    Friend WithEvents cmdC As Button
+    Friend WithEvents cmdEqual As Button
+    Friend WithEvents cmdBackspace As Button
+    Friend WithEvents cmdDecimal As Button
+    Friend WithEvents cmdAddition As Button
+    Friend WithEvents cmd0 As Button
+    Friend WithEvents cmdSign As Button
+    Friend WithEvents cmdInverse As Button
+    Friend WithEvents cmd3 As Button
+    Friend WithEvents cmdSubstraction As Button
+    Friend WithEvents cmd2 As Button
+    Friend WithEvents cmd1 As Button
+    Friend WithEvents cmdSqrt As Button
+    Friend WithEvents cmdDivision As Button
+    Friend WithEvents cmd6 As Button
+    Friend WithEvents cmdMultiply As Button
+    Friend WithEvents cmd5 As Button
+    Friend WithEvents cmdPowerOf As Button
+    Friend WithEvents cmd4 As Button
+    Friend WithEvents cmd9 As Button
+    Friend WithEvents cmd8 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents cmdAkarPangkat3 As Button
+    Friend WithEvents cmdFactorial As Button
+End Class
---
+++ Pertemuan_1/Kalkulator2_71230982/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Kalkulator2_71230982")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Kalkulator2_71230982")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1edef6b0-6db8-4d67-a782-64d0867a19d4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
