00c8b2700e51905ebcb28f85d1d7df0fb9eb7779
rainiefch <143920728+rainiefch@users.noreply.github.com>
2025-03-19 09:13:47 +0700

    Add files via upload

---
+++ Parkiran/Parkiran/ModuleDB.vb
@@ -0,0 +1,20 @@
+﻿Imports MySql.Data.MySqlClient
+Module ModuleDB
+    Public pengguna As String = "anton"
+    Public password As String = "anton"
+    Public myStrCon As MySqlConnectionStringBuilder = New MySqlConnectionStringBuilder()
+    Public myConn As MySqlConnection
+    Public myCommand As MySqlCommand
+    Public myDataReader As MySqlDataReader
+    Public myDataAdapter As MySqlDataAdapter
+    Public jns As String = String.Empty
+    Public tbluser As String = "users" 'nama tabel user
+    Public bts As Integer = 5
+    Public Sub CreateConnection()
+        myStrCon.UserID = "root"
+        myStrCon.Server = "localhost"
+        myStrCon.Password = ""
+        myStrCon.Database = "parkiran" 'sesuaikan nama databasenya
+        myConn = New MySqlConnection(myStrCon.ToString)
+    End Sub
+End Module
